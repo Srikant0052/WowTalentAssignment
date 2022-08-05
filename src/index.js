b47caccf4eceae9ce1621aa3e18f1ea97d51a758
Srikant0052 <srikant0052@gmail.com>
2022-08-05 12:02:45 +0530

    index file added

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const route = require('./routes/route');
+const multer = require('multer');
+const app = express();
+
+app.use(multer().any());
+app.use(express.json());
+
+
+mongoose.connect("mongodb+srv://umed007-sable:4Q9gJnzBwxbCeaJP@cluster0.wunsw.mongodb.net/Srikant_DB?authSource=admin&replicaSet=atlas-63xe24-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true", {
+    useNewUrlParser: true
+})
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+app.use('/', route)
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
